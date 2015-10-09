30e1847ce411767206e30bf132b75e465a714ad6
ianngiaw <ian@u.nus.edu>
2015-10-10 02:39:24 +0800

    init facebook and add module dependencies

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'facebook', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -20,6 +20,10 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       StatusBar.styleDefault();
     }
   });
+})
+
+.config(function(FacebookProvider) {
+  FacebookProvider.init('1674828996062928');
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
