3bf807acf8e450bc8637b4c3bd453e78b5318aa1
Sebastian Quek <sebq13@gmail.com>
2015-10-10 12:16:46 +0800

    [#14] Added settings list view and settings route

--- www/js/app.js
+++ www/js/app.js
@@ -71,6 +71,16 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     }
   })
+
+  .state('app.settings', {
+    url: '/settings',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/settings.html',
+        controller: 'SettingsCtrl'
+      }
+    }
+  })
   ;
 
   // if none of the above states are matched, use this as the fallback
