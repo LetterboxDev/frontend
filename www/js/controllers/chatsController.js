ec8d362cf6ada36b241da33c5085d14cb88efefd
Sebastian Quek <sebq13@gmail.com>
2015-10-10 10:11:57 +0800

    Updated mock data for chats

--- www/js/controllers/chatsController.js
+++ www/js/controllers/chatsController.js
@@ -2,9 +2,8 @@ angular.module('starter.controllers')
 
 .controller('ChatsCtrl', function($scope) {
   $scope.chats = [
-    { from: 'Person 1', id: 1 },
-    { from: 'Person 2', id: 2 },
-    { from: 'Person 3', id: 3 },
+    { id: 1, from: 'Cassandra Ong', profile_pic: "http://semantic-ui.com/images/avatar/large/jenny.jpg", last_message: "Hello!", last_activity: "1h"},
+    { id: 2, from: 'Michelle Lee', profile_pic: "http://semantic-ui.com/images/avatar/large/helen.jpg", last_message: "Ok great! See you later!", last_activity: "2h"},
   ];
 });
 
