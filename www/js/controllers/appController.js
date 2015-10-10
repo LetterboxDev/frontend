c48b5f5639a9a62a723e42d053a5bfce0f5a369f
ianngiaw <ian@u.nus.edu>
2015-10-10 16:11:58 +0800

    added authentication, currently stored in localStorage

--- www/js/controllers/appController.js
+++ www/js/controllers/appController.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, Facebook, $cordovaOauth) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, Facebook, $cordovaOauth, backend) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -15,23 +15,41 @@ angular.module('starter.controllers', [])
     hardwareBackButtonClose: false
   }).then(function(modal) {
     $scope.modal = modal;
-    $scope.modal.show();
+    if (!window.localStorage.getItem('token')) {
+      $scope.modal.show();      
+    }
   });
+
+  $scope.authenticateToken = function(fbToken) {
+    $scope.testing = 'fb_token: ' + fbToken;
+    backend.auth(fbToken).$promise.then(function(success) {
+      window.localStorage.setItem('token', success.letterbox_token);
+      window.localStorage.setItem('firstName', success.user.firstName);
+      window.localStorage.setItem('hashedId', success.user.hashedId);
+      window.localStorage.setItem('isRegistered', success.user.isRegistered);
+      $scope.modal.hide();
+    }, function(error) {
+      // error something went wrong
+    });
+  };
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     if (!window.cordova) {
       Facebook.login(function(response) {
+        if (response.status === 'connected') {
+          $scope.authenticateToken(response.authResponse.accessToken);
+        } else {
+          // error, something went wrong
+        }
         // make request to server if new or returning, get letterbox_token
-        $scope.modal.hide();
       });
     } else {
       $cordovaOauth.facebook('1674828996062928', ['public_profile','user_birthday','user_education_history','user_work_history']).then(function(res) {
         // make request to server if new or returning, get letterbox_token
-        $scope.modal.hide();
+        $scope.authenticateToken(res.access_token);
       }, function(err) {
         // inform of error
-        $scope.modal.hide();
       });
     }
   };
