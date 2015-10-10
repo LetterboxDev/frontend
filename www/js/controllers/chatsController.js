1347884c18618794c34ff4006913b32d7ea29c0b
Lei Mingyu <uygnim.iel@gmail.com>
2015-10-10 23:28:34 +0800

    use notifications service to manage all the notifications and show correct notification number

--- www/js/controllers/chatsController.js
+++ www/js/controllers/chatsController.js
@@ -1,10 +1,12 @@
 angular.module('starter.controllers')
 
-.controller('ChatsCtrl', function($scope, $state) {
+.controller('ChatsCtrl', function($scope, $state, NotificationsService) {
   $scope.chats = [
     { id: 1, from: 'Cassandra Ong', profile_pic: "http://semantic-ui.com/images/avatar/large/jenny.jpg", last_message: "Hello!", last_activity: "1h"},
     { id: 2, from: 'Michelle Lee', profile_pic: "http://semantic-ui.com/images/avatar/large/stevie.jpg", last_message: "Ok great! See you later!", last_activity: "2h"},
   ];
+
+  $scope.numberOfNotifications = NotificationsService.getNumberOfNotifications();
 
   // navigation
   $scope.goNotifications = function() {
--- www/js/controllers/notificationsController.js
+++ www/js/controllers/notificationsController.js
@@ -1,9 +1,7 @@
 angular.module('starter.controllers')
 
-.controller('NotificationsCtrl', function($scope, $state) {
-  $scope.notifications = [
-    { id: 1, from: 'Helen Heng', profile_pic: "http://semantic-ui.com/images/avatar/large/helen.jpg", message: "answered your questions, click here to see her responses", timestamp: "1h"},
-  ];
+.controller('NotificationsCtrl', function($scope, $state, NotificationsService) {
+  $scope.notifications = NotificationsService.getNotificationsList();
 
   // navigation
   $scope.goHome = function() {
