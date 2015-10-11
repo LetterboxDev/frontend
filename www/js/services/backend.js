caf9a6d457c632f601d570f6f48e225270d67c9b
ianngiaw <ian@u.nus.edu>
2015-10-11 14:31:13 +0800

    added renew token and update location to backend service

--- www/js/services/backend.js
+++ www/js/services/backend.js
@@ -4,8 +4,10 @@ angular.module('starter.services')
   var URL = 'http://ec2-52-74-138-177.ap-southeast-1.compute.amazonaws.com';
 
   var authPath = '/auth';
+  var renewPath = '/auth/renew'
   var userSelfPath = '/user/self';
   var otherUserPath = '/user/id/:UserId';
+  var updateLocationPath = '/user/location';
   var questionsPath = '/questions';
   var roomsPath = '/rooms';
 
@@ -15,10 +17,36 @@ angular.module('starter.services')
     }
   });
 
+  var renewToken = $resource(URL.concat(renewPath), {}, {
+    get: {
+      method: 'GET'
+    }
+  });
+
+  var updateUserLocation = $resource(URL.concat(updateLocationPath), {}, {
+    updateLocation: {
+      method: 'PUT',
+      params: {
+        letterbox_token: '@token'
+      }
+    }
+  });
+
   backend.auth = function(fbToken) {
     return auth.get({fb_token: fbToken});
   };
 
+  backend.renewToken = function(token) {
+    return renewToken.get({letterbox_token: token});
+  };
+
+  backend.updateUserLocation = function(token, latitude, longitude, successPromise) {
+    locationUpdater = new updateUserLocation();
+    locationUpdater.token = token;
+    locationUpdater.latitude = latitude;
+    locationUpdater.longitude = longitude;
+    return locationUpdater.$updateLocation(successPromise);
+  };
 
   return backend;
 });
