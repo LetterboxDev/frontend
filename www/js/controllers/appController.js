763621043f76accab36dbaf429bd2c4a9c0a2fb3
Sebastian Quek <sebq13@gmail.com>
2015-10-11 16:50:52 +0800

    Shifted login/onboarding logic to onboarding controller and created login and onboarding routes

--- www/js/controllers/appController.js
+++ www/js/controllers/appController.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', ['ionic.contrib.ui.cards'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, Facebook, $cordovaOauth, backend) {
+.controller('AppCtrl', function($scope, $state) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -9,90 +9,11 @@ angular.module('starter.controllers', ['ionic.contrib.ui.cards'])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $ionicModal.fromTemplateUrl('templates/onboard1.html', {
-    scope: $scope,
-    hardwareBackButtonClose: false
-  }).then(function(onboardModal) {
-    $scope.onboardModal1 = onboardModal;
-  });
-
-  $ionicModal.fromTemplateUrl('templates/onboard2.html', {
-    scope: $scope,
-    hardwareBackButtonClose: false
-  }).then(function(onboardModal) {
-    $scope.onboardModal2 = onboardModal;
-  });
-
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope,
-    hardwareBackButtonClose: false
-  }).then(function(loginModal) {
-    $scope.loginModal = loginModal;
-    if (!window.localStorage.getItem('token')) {
-      $scope.loginModal.show();
-    }
-    $scope.beginOnboarding();
-  });
-
-  $scope.beginOnboarding = function() {
-    if (window.localStorage.getItem('token') && window.localStorage.getItem('isRegistered') === 'false') {
-      $scope.onboardModal1.show();
-    }
-  };
-
-  $scope.selectGender = function(gender) {
-    $scope.gender = gender;
-    console.log($scope.gender);
+  if (!window.localStorage.getItem('token')) {
+    $state.go('login');
+  } else if (window.localStorage.getItem('isRegistered') === 'false') {
+    $state.go('onboarding', {onboardStep: 1});
   }
-
-  $scope.selectGenderPref = function(gender) {
-    $scope.genderPref = gender;
-    console.log($scope.genderPref);
-  }
-
-  $scope.completeOnboardingStep1 = function() {
-    $scope.onboardModal1.hide();
-    $scope.onboardModal2.show();
-  };
-
-  $scope.completeOnboarding = function() {
-    window.localStorage.setItem('isRegistered', 'true');
-    $scope.onboardModal2.hide();
-  };
-
-  $scope.authenticateToken = function(fbToken) {
-    $scope.testing = 'fb_token: ' + fbToken;
-    backend.auth(fbToken).$promise.then(function(success) {
-      window.localStorage.setItem('token', success.letterbox_token);
-      window.localStorage.setItem('firstName', success.user.firstName);
-      window.localStorage.setItem('hashedId', success.user.hashedId);
-      window.localStorage.setItem('isRegistered', success.user.isRegistered);
-      $scope.loginModal.hide();
-      $scope.beginOnboarding();
-    }, function(error) {
-      // error something went wrong
-    });
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    if (!window.cordova) {
-      Facebook.login(function(response) {
-        if (response.status === 'connected') {
-          $scope.authenticateToken(response.authResponse.accessToken);
-        } else {
-          // error, something went wrong
-        }
-        // make request to server if new or returning, get letterbox_token
-      });
-    } else {
-      $cordovaOauth.facebook('1674828996062928', ['public_profile','user_birthday','user_education_history','user_work_history']).then(function(res) {
-        // make request to server if new or returning, get letterbox_token
-        $scope.authenticateToken(res.access_token);
-      }, function(err) {
-        // inform of error
-      });
-    }
-  };
+  
 });
 
---
+++ www/js/controllers/onboardingController.js
@@ -0,0 +1,95 @@
+angular.module('starter.controllers')
+
+.controller('OnboardingCtrl', function($scope, $state, $timeout, Facebook, $cordovaOauth, backend) {
+
+  // Login logic
+
+  $scope.authenticateToken = function(fbToken) {
+    $scope.testing = 'fb_token: ' + fbToken;
+    backend.auth(fbToken).$promise.then(function(success) {
+      window.localStorage.setItem('token', success.letterbox_token);
+      window.localStorage.setItem('firstName', success.user.firstName);
+      window.localStorage.setItem('hashedId', success.user.hashedId);
+      window.localStorage.setItem('isRegistered', success.user.isRegistered);
+      $scope.beginOnboarding();
+    }, function(error) {
+      // error something went wrong
+    });
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    if (!window.cordova) {
+      Facebook.login(function(response) {
+        if (response.status === 'connected') {
+          $scope.authenticateToken(response.authResponse.accessToken);
+        } else {
+          // error, something went wrong
+        }
+      });
+    } else {
+      $cordovaOauth.facebook('1674828996062928', ['public_profile','user_birthday','user_education_history','user_work_history']).then(function(res) {
+        $scope.authenticateToken(res.access_token);
+      }, function(err) {
+        // inform of error
+      });
+    }
+  };
+
+  $scope.beginOnboarding = function() {
+    if (window.localStorage.getItem('token') &&
+        window.localStorage.getItem('isRegistered') === 'false') {
+      $state.go('onboarding', {onboardStep: 1});
+    }
+  };
+
+
+  // Onboarding step 1 logic
+
+  $scope.selectGender = function(gender) {
+    if (gender === 'male' || gender === 'female') {
+      if (gender === 'male') {
+        this.isMale = true;
+        this.isFemale = false;
+      } else {
+        this.isMale = false;
+        this.isFemale = true;
+      }
+      window.localStorage.setItem('gender', gender);
+    }
+  }
+
+  $scope.selectGenderPref = function(gender) {
+    if (gender === 'male' || gender === 'female') {
+      if (gender === 'male') {
+        this.likesMales = true;
+        this.likesFemales = false;
+      } else {
+        this.likesMales = false;
+        this.likesFemales = true;
+      }
+      window.localStorage.setItem('genderPref', gender);
+    }
+  }
+
+  $scope.completeOnboardingStep1 = function() {
+    if (!window.localStorage.getItem('gender') ||
+        !window.localStorage.getItem('genderPref')) {
+      // TODO handle error
+    } else {
+      $state.go('onboarding', {onboardStep: 2});
+    }
+  };
+
+
+  // Onboarding step 2 logic
+
+  $scope.getNewQn = function(qnNum) {
+    // get new qn and replace the existing qn
+  }
+
+  $scope.completeOnboarding = function() {
+    window.localStorage.setItem('isRegistered', 'true');
+    $state.go('app.home');
+  };
+});
