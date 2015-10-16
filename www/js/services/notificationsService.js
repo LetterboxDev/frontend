ada805119a229e83d1872d5e3159e7c0a947305a
ianngiaw <ian@u.nus.edu>
2015-10-17 01:08:35 +0800

    updated notificationservice with backend stuff

--- www/js/services/notificationsService.js
+++ www/js/services/notificationsService.js
@@ -1,15 +1,40 @@
 angular.module('starter.services')
 
-.service('NotificationsService', function($q) {
-  var notifications = [
-    { id: 1, from: 'Helen Heng', profile_pic: "http://semantic-ui.com/images/avatar/large/helen.jpg", message: "answered your questions, click here to see her responses", timestamp: "1h"},
-  ];
-
-  // some backend link stuff here
+.service('NotificationsService', function($q, backend) {
+  function convertLetterToNotif(letter) {
+    var questionsAnswers = [];
+    for (var i = 0; i < letter.LetterAnswers.length; i++) {
+      var question = letter.LetterAnswers[i];
+      questionsAnswers.push({
+        option0: question.WyrQuestion.option0,
+        option1: question.WyrQuestion.option1,
+        answer: question.WyrQuestion.answer
+      });
+    }
+    return {
+      id: letter.hash,
+      isRead: letter.isRead,
+      createdAt: letter.createdAt,
+      from: letter.UserAccount.firstName,
+      fromGender: letter.UserAccount.gender,
+      profilePicThumb: letter.UserAccount.pictureThumb,
+      profilePicMed: letter.UserAccount.pictureMed,
+      questionsAnswers: questionsAnswers
+    };
+  }
 
   return {
     getNotificationsList: function() {
-      return notifications;
+      var deferred = $q.defer();
+      backend.getAllLetters().$promise
+      .then(function(letters) {
+        var result = [];
+        letters.forEach(function(letter) {
+          result.push(convertLetterToNotif(letter));
+        });
+        deferred.resolve(result);
+      });
+      return deferred.promise;
     },
 
     getNumberOfNotifications: function() {
