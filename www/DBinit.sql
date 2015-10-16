9575a8272008d475eddcc2dba7afc380d947d7d8
ianngiaw <ian@u.nus.edu>
2015-10-16 18:17:08 +0800

    updated db to include rooms

--- www/DBinit.sql
+++ www/DBinit.sql
@@ -1,5 +1,13 @@
+CREATE TABLE rooms (
+hash CHAR(32) PRIMARY KEY,
+userId CHAR(32) NOT NULL,
+userName VARCHAR(256) NOT NULL,
+thumbnail TEXT NOT NULL,
+profilePicture TEXT NOT NULL
+);
+
 CREATE TABLE messages ( 
-roomHash CHAR(32) NOT NULL,
+roomHash CHAR(32) NOT NULL REFERENCES rooms(hash),
 sender VARCHAR(256) NOT NULL,
 content TEXT NOT NULL,
 timeSent INT NOT NULL,
