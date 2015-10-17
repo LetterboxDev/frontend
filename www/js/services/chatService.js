884679d3a14d71864590b2592dc973c18757f675
ianngiaw <ian@u.nus.edu>
2015-10-17 23:54:56 +0800

    changed data format

--- www/js/services/chatService.js
+++ www/js/services/chatService.js
@@ -1,10 +1,18 @@
 angular.module('starter.services')
 
-.service('ChatService', function($q, RoomsService) {
+.service('ChatService', function($q, RoomsService, backend) {
   return {
     getRecipientName: function(chatId) {
       var dfd = $q.defer();
-      dfd.resolve("Cassandra");
+      var room = RoomsService.getRoom(chatId);
+      if (room !== null) {
+        dfd.resolve(room.userName);
+      } else {
+        backend.getSingleRoom(chatId).$promise.then(function(room) {
+          dfd.resolve(room.userName);
+        });
+      }
+      
       return dfd.promise;
     },
     getMessagesFromBackend: function(chatId) {
@@ -14,61 +22,61 @@ angular.module('starter.services')
         id: 1,
         content: "hi",
         timestamp: "3m",
-        received: true
+        isOwner: true
       },
       {
         id: 2,
         content: "hello!",
         timestamp: "2m",
-        received: false
+        isOwner: false
       },
       {
         id: 1,
         content: "hi",
         timestamp: "3m",
-        received: true
+        isOwner: true
       },
       {
         id: 1,
         content: "hi",
         timestamp: "3m",
-        received: true
+        isOwner: true
       },
       {
         id: 1,
         content: "hi",
         timestamp: "3m",
-        received: true
+        isOwner: true
       },
       {
         id: 1,
         content: "hi",
         timestamp: "3m",
-        received: true
+        isOwner: true
       },
       {
         id: 1,
         content: "hi",
         timestamp: "3m",
-        received: true
+        isOwner: true
       },
       {
         id: 1,
         content: "hi",
         timestamp: "3m",
-        received: true
+        isOwner: true
       },
       {
         id: 1,
         content: "hi",
         timestamp: "3m",
-        received: true
+        isOwner: true
       },
       {
         id: 1,
         content: "hi",
         timestamp: "3m",
-        received: true
+        isOwner: true
       },
       ])
       return dfd.promise;
