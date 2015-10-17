e4046acf5195e3d93ef9da9e63b0f72db5703dfe
Lei Mingyu <uygnim.iel@gmail.com>
2015-10-18 00:15:34 +0800

    add flip card directive back

---
+++ www/js/directives/flipCardDirective.js
@@ -0,0 +1,17 @@
+angular.module('starter.directives', [])
+
+.directive('flipCard', function($document) {
+  return {
+    restrict: 'A',
+    link: function($scope, $element, $attr) {
+      function selectFirst(selector) {
+        return angular.element($element[0].querySelectorAll(selector));
+      }
+
+      selectFirst('.button-flip').on('touch', function(e) {
+        selectFirst('.flipper-container').toggleClass('hover');
+      });
+    }
+  };
+});
+
