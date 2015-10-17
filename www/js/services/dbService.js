3ea09f7de248293b7cedfe805055dcb253940665
ianngiaw <ian@u.nus.edu>
2015-10-17 16:45:58 +0800

    Updated DbService to update itself whenever a get is called and calls eventbus if any changes

--- www/js/services/dbService.js
+++ www/js/services/dbService.js
@@ -1,18 +1,26 @@
 angular.module('starter.services')
 
-.service('DbService', function($q, $window) {
+.service('DbService', function($q, $window, socket, backend, eventbus) {
   var db = {isInitialized: false};
   var DbService = {};
 
   function checkInit(deferred) {
-    if (!db.isInitialized || typeof db.sqlite === 'undefined') deferred.reject({err: 'DbService not initialized'});
+    if (!DbService.isInitialized()) deferred.reject({err: 'DbService not initialized'});
   }
+
+  DbService.isInitialized = function() {
+    return db.isInitialized && typeof db.sqlite !== 'undefined';
+  };
 
   DbService.init = function() {
     if (window.cordova) {
       document.addEventListener("deviceready", function() {
-        db.sqlite = $window.sqlitePlugin.openDatabase({name: 'letterbox.db', createFromLocation: 1});
-        db.isInitialized = true;
+        db.sqlite = $window.sqlitePlugin.openDatabase({name: 'letterbox.db'});
+        db.sqlite.transaction(function(tx) {
+          tx.executeSql('CREATE TABLE IF NOT EXISTS rooms (hash CHAR(32) PRIMARY KEY, userId CHAR(32) NOT NULL, userName VARCHAR(256) NOT NULL, thumbnail TEXT NOT NULL, profilePicture TEXT NOT NULL)');
+          tx.executeSql('CREATE TABLE IF NOT EXISTS messages (roomHash CHAR(32) NOT NULL REFERENCES rooms(hash), sender VARCHAR(256) NOT NULL, content TEXT NOT NULL, timeSent INT NOT NULL, isRead BOOLEAN NOT NULL DEFAULT 0, PRIMARY KEY (roomHash, sender, timeSent))');
+          db.isInitialized = true;
+        });
       }, false);
     }
   };
@@ -22,8 +30,16 @@ angular.module('starter.services')
     checkInit(deferred);
 
     db.sqlite.transaction(function(tx) {
-      tx.executeSql("INSERT INTO rooms VALUES (?,?,?,?,?)", [roomHash, userHash, userFirstName, thumbnailPic, mediumPic], function(tx, res) {
-        deferred.resolve(res);
+      tx.executeSql("SELECT COUNT(*) AS cnt FROM rooms WHERE hash=?", [roomHash], function(tx, res) {
+        if (!res.rows.item(0).cnt) {
+          tx.executeSql("INSERT INTO rooms VALUES (?,?,?,?,?)", [roomHash, userHash, userFirstName, thumbnailPic, mediumPic], function(tx, res) {
+            deferred.resolve(res);
+          });
+        } else {
+          deferred.reject({
+            error: 'room already exists'
+          });
+        }
       });
     });
     return deferred.promise;
@@ -52,14 +68,44 @@ angular.module('starter.services')
     return deferred.promise;
   };
 
+  function getRoomsFromDb() {
+    var deferred = $q.defer();
+    db.sqlite.transaction(function(tx) {
+      tx.executeSql("SELECT * FROM rooms", [], function(tx, res) {
+        var rooms = [];
+        for (var i = 0; i < res.rows.length; i++) {
+          var row = res.rows.item(i);
+          rooms.push({
+            hash: row.hash,
+            userId: row.userId,
+            userName: row.userName,
+            thumbnail: row.thumbnail,
+            profilePicture: row.profilePicture
+          });
+        }
+        deferred.resolve(rooms);
+      });
+    });
+    return deferred.promise;
+  }
+
+  function updateRoomsInDb() {
+    backend.getRooms().$promise.then(function(rooms) {
+      rooms.forEach(function(room) {
+        DbService.addRoom(room.hash, room.userId, room.userName, room.thumbnail, room.profilePicture).then(function(success) {
+          eventbus.call('roomsUpdated', rooms);
+        });
+      });
+    });
+  }
+
   DbService.getRooms = function() {
     var deferred = $q.defer();
     checkInit(deferred);
-    db.sqlite.transaction(function(tx) {
-      tx.executeSql("SELECT * FROM rooms", [], function(tx, res) {
-        deferred.resolve(res);
-      });
-    });
+    if (socket.isConnected()) {// Is connected to Internet and backend is awake
+      updateRoomsInDb();
+    }
+    getRoomsFromDb().then(deferred.resolve);
     return deferred.promise;
   };
 
@@ -78,7 +124,7 @@ angular.module('starter.services')
     var deferred = $q.defer();
     checkInit(deferred);
     db.sqlite.transaction(function(tx) {
-      tx.executeSql("SELECT roomHash, COUNT(*) FROM messages GROUP BY roomHash, isRead HAVING isRead=0", [], function(tx, res) {
+      tx.executeSql("SELECT roomHash, COUNT(*) as cnt FROM messages GROUP BY roomHash, isRead HAVING isRead=0", [], function(tx, res) {
         deferred.resolve(res);
       });
     });
