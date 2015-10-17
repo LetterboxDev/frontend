59e0154c5a2b5a8316f460edaf720057a129fa7d
ianngiaw <ian@u.nus.edu>
2015-10-17 15:36:23 +0800

    removed getting of other user data when getting rooms since it is returned in the call now

--- www/js/controllers/chatsController.js
+++ www/js/controllers/chatsController.js
@@ -5,20 +5,14 @@ angular.module('starter.controllers')
 
   backend.getRooms().$promise.then(function(res) {
     res.forEach(function(chat) {
-      var partnerId = (chat.user1 === window.localStorage.getItem('hashedId')) ? chat.user2 : chat.user1;
-      backend.getOtherUser(partnerId).$promise.then(function(res) {
-        var newChat = {
-          hash: chat.hash,
-          from: res.firstName,
-          profile_pic: res.pictureThumb,
-          last_message: "I'm hungry",
-          last_activity: "1h"
-        };
-        $scope.chats.push(newChat);
-      }, function(err) {
-
-      });
-
+      var newChat = {
+        hash: chat.hash,
+        from: chat.userName,
+        profile_pic: chat.thumbnail,
+        last_message: "I'm hungry",
+        last_activity: "1h"
+      };
+      $scope.chats.push(newChat);
     });
   }, function(err) {
 
