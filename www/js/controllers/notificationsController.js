5480255abab446fd746f6169263984d79696fae2
ianngiaw <ian@u.nus.edu>
2015-10-17 12:27:33 +0800

    added some functionality to notificationsctrl

--- www/js/controllers/notificationsController.js
+++ www/js/controllers/notificationsController.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers')
 
-.controller('NotificationsCtrl', function($scope, $state, NotificationsService, eventbus) {
+.controller('NotificationsCtrl', function($scope, $state, $ionicModal, NotificationsService, eventbus, backend) {
   $scope.notifications = [];
 
   function refreshNotifications() {
@@ -11,4 +11,37 @@ angular.module('starter.controllers')
 
   refreshNotifications();
   eventbus.registerListener('letterReceived', refreshNotifications);
+
+  $ionicModal.fromTemplateUrl('templates/letter-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.selectNotification = function(notification) {
+    console.log(notification);
+    notification.isRead = true;
+    $scope.selectedLetter = notification;
+    $scope.modal.show();
+    backend.markLetterAsRead(notification.id);
+  };
+
+  $scope.closeLetterModal = function() {
+    $scope.modal.hide();
+  };
+
+  $scope.rejectLetter = function(letter) {
+    // rejects the letter and refresh notifs
+    backend.rejectLetter(letter.id, function() {
+      $scope.closeLetterModal();
+      refreshNotifications();
+    });
+  };
+
+  $scope.approveLetter = function(letter) {
+    // approves letter, opens a chat and refresh notifs
+    backend.approveLetter(letter.id, refreshNotifications);
+    $scope.closeLetterModal();
+  };
 });
