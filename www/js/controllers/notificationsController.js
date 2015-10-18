d5cee2e1a56be4f77cbb32bfc9b7cfc2c614cd12
Sebastian Quek <sebq13@gmail.com>
2015-10-18 09:42:45 +0800

    Merged stashed files

--- www/js/controllers/notificationsController.js
+++ www/js/controllers/notificationsController.js
@@ -12,35 +12,35 @@ angular.module('letterbox.controllers')
   refreshNotifications();
   eventbus.registerListener('letterReceived', refreshNotifications);
 
-  $ionicModal.fromTemplateUrl('templates/letter-modal.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+  // $ionicModal.fromTemplateUrl('templates/letter-modal.html', {
+  //   scope: $scope,
+  //   animation: 'slide-in-up'
+  // }).then(function(modal) {
+  //   $scope.modal = modal;
+  // });
 
   $scope.selectNotification = function(notification) {
     notification.isRead = true;
     $scope.selectedLetter = notification;
-    $scope.modal.show();
     backend.markLetterAsRead(notification.id);
+    $state.go('app.response', {responseId: notification.id});
   };
+  
+  // $scope.closeLetterModal = function() {
+  //   $scope.modal.hide();
+  // };
 
-  $scope.closeLetterModal = function() {
-    $scope.modal.hide();
-  };
+  // $scope.rejectLetter = function(letter) {
+  //   // rejects the letter and refresh notifs
+  //   backend.rejectLetter(letter.id, function() {
+  //     $scope.closeLetterModal();
+  //     refreshNotifications();
+  //   });
+  // };
 
-  $scope.rejectLetter = function(letter) {
-    // rejects the letter and refresh notifs
-    backend.rejectLetter(letter.id, function() {
-      $scope.closeLetterModal();
-      refreshNotifications();
-    });
-  };
-
-  $scope.approveLetter = function(letter) {
-    // approves letter, opens a chat and refresh notifs
-    backend.approveLetter(letter.id, refreshNotifications);
-    $scope.closeLetterModal();
-  };
+  // $scope.approveLetter = function(letter) {
+  //   // approves letter, opens a chat and refresh notifs
+  //   backend.approveLetter(letter.id, refreshNotifications);
+  //   $scope.closeLetterModal();
+  // };
 });
