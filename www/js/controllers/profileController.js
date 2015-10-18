fccb8c7f1f59e07a75eb7a0a2ca7e456172194e1
Sebastian Quek <sebq13@gmail.com>
2015-10-18 14:32:53 +0800

    Added method to select question option

--- www/js/controllers/profileController.js
+++ www/js/controllers/profileController.js
@@ -2,4 +2,9 @@ angular.module('letterbox.controllers')
 
 .controller('ProfileCtrl', function($scope, profile, backend, eventbus) {
   $scope.profile = profile;
+  $scope.selectOption = function(question, option) {
+    var index = $scope.profile.questions.indexOf(question);
+    question.answer = option;
+    $scope.profile.questions[index] = question;
+  }
 });
