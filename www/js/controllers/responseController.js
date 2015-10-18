4aa43c8e180eb78c731ea2f942cb4c8d02c9373a
Sebastian Quek <sebq13@gmail.com>
2015-10-18 12:23:52 +0800

    Accepting a response now redirects to the chat

--- www/js/controllers/responseController.js
+++ www/js/controllers/responseController.js
@@ -1,6 +1,6 @@
 angular.module('letterbox.controllers')
 
-.controller('ResponseCtrl', function($scope, $state, $stateParams, NotificationsService) {
+.controller('ResponseCtrl', function($scope, $state, $stateParams, backend, NotificationsService, eventbus) {
 
   function getNotification() {
     NotificationsService.getNotificationFromId($stateParams.responseId).then(function(notification) {
@@ -13,14 +13,15 @@ angular.module('letterbox.controllers')
   $scope.rejectLetter = function(letter) {
     // rejects the letter and refresh notifs
     backend.rejectLetter(letter.id, function() {
-      // $scope.closeLetterModal();
-      // refreshNotifications();
+      eventbus.call('letterReceived'); //TODO this triggers an unrelated prompt
     });
   };
 
   $scope.approveLetter = function(letter) {
     // approves letter, opens a chat and refresh notifs
-    backend.approveLetter(letter.id, refreshNotifications);
-    // $scope.closeLetterModal();
+    backend.approveLetter(letter.id, function() {
+      eventbus.call('letterReceived'); //TODO this triggers an unrelated prompt
+      $state.go('app.chat', {chatId: letter.id});
+    });
   };
 });
