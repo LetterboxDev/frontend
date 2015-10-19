8e46f3f4c704d0038fe06400471da6f6cde862a4
Lei Mingyu <uygnim.iel@gmail.com>
2015-10-19 18:16:21 +0800

    disable back button when user go to notification page

--- www/js/controllers/homeController.js
+++ www/js/controllers/homeController.js
@@ -1,7 +1,13 @@
 angular.module('letterbox.controllers')
 
 
-.controller('HomeCtrl', function($scope, $state, NotificationsService) {
+.controller('HomeCtrl', function($scope, $state, $ionicHistory, NotificationsService) {
+  $scope.numberOfNotifications = 0;
+
+  $ionicHistory.nextViewOptions({
+    disableBack: true
+  });
+
   NotificationsService.getNumberOfNotifications()
     .then(function(numberOfNotifications) {
       $scope.numberOfNotifications = numberOfNotifications;
