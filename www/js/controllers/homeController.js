e12e98873317184d66bccbfcb39cac2e52e95d2b
Lei Mingyu <uygnim.iel@gmail.com>
2015-10-19 18:06:31 +0800

    add NotificationsService back to home controller for displaying the bell

--- www/js/controllers/homeController.js
+++ www/js/controllers/homeController.js
@@ -1,7 +1,11 @@
 angular.module('letterbox.controllers')
 
 
-.controller('HomeCtrl', function($scope, $state) {
+.controller('HomeCtrl', function($scope, $state, NotificationsService) {
+  $scope.numberOfNotifications = NotificationsService.getNumberOfNotifications();
 
+  $scope.goNotifications = function() {
+    $state.go('app.notifications');
+  };
 });
 
