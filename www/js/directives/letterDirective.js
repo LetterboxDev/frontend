7cdec8f9fc71b194adc657cda07a8abb74439d6a
Bili Xu <xubili93@gmail.com>
2015-10-22 20:20:33 +0800

    Include letter directive to show different question in stages

--- www/js/directives/letterDirective.js
+++ www/js/directives/letterDirective.js
@@ -1,12 +1,44 @@
 angular.module('letterbox.directives')
 
-.directive('letter', function($document) {
+.directive('letter', function($document, letterService) {
   return {
     restrict: 'A',
     link: function($scope, $element, $attr) {
-      angular.element($element[0].querySelectorAll('.button-flip')).on('touch', function(e) {
-        $element.toggleClass('flipped');
+      var questions = letterService.targetUserCard.questions;
+      var curr = 0;
+      var max = questions.length - 1;
+
+      angular.element($element[0].querySelectorAll('.button-next')).on('touch', function(e) {
+        if (curr < max) {
+          curr++;
+          updateQuestion($scope, curr, questions);
+        }
+        resetClass($scope);
+      });
+
+      angular.element($element[0].querySelectorAll('.button-prev')).on('touch', function(e) {
+        if (curr > 0) {
+          curr--;
+          updateQuestion($scope, curr, questions);
+        }
+        resetClass($scope);
       });
     }
   };
+
+  /**
+   * Helper functions
+   */
+
+  // Removes active class from .left and .right
+  function resetClass($scope) {
+    $scope.selectedTab = undefined;
+    $scope.$apply();
+  }
+
+  // Updates the question contents
+  function updateQuestion($scope, curr, questions) {
+    $scope.currentQuestion = questions[curr];
+    $scope.$apply();
+  }
 });
