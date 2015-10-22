b7592ff3cc54c4f29b5b75444434adc99b6aed88
Bili Xu <xubili93@gmail.com>
2015-10-22 20:37:01 +0800

    Remove directives and move all question logic into controller

--- www/js/controllers/letterController.js
+++ www/js/controllers/letterController.js
@@ -2,10 +2,28 @@ angular.module('letterbox.controllers')
 
 .controller('LetterCtrl', function($scope, $state, $ionicHistory, backend, letterService) {
   var targetUser = letterService.targetUserCard;
+  var questions = targetUser.questions;
+  var curr = 0;
+  var max = questions.length - 1;
 
   $scope.userName = targetUser.name;
-
   $scope.currentQuestion = targetUser.questions[0];
+
+  $scope.nextQuestion = function() {
+    if (curr < max) {
+      curr++;
+      resetClass();
+      $scope.currentQuestion = targetUser.questions[curr];
+    }
+  };
+
+  $scope.prevQuestion = function() {
+    if (curr > 0) {
+      curr--;
+      resetClass();
+      $scope.currentQuestion = targetUser.questions[curr];
+    }
+  };
 
   $scope.closeLetter = function() {
     $ionicHistory.nextViewOptions({
@@ -17,5 +35,14 @@ angular.module('letterbox.controllers')
   $scope.submitQuestionAnswer = function() {
     backend.sendALetter($scope.cards[0].hashedId, $scope.cards[0].questions);
   };
+
+  /**
+   * Helper functions
+   */
+
+  // Removes active class from .left and .right
+  function resetClass() {
+    $scope.selectedTab = undefined;
+  }
 });
 
