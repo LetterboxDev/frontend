6ac1c3e55f1da8f09656a1bb0f2eb99e8fd4a664
Lei Mingyu <uygnim.iel@gmail.com>
2015-10-25 15:48:33 +0800

    bind slider with pref modal

--- www/js/controllers/settingsController.js
+++ www/js/controllers/settingsController.js
@@ -1,15 +1,26 @@
 angular.module('letterbox.controllers')
 
 .controller('SettingsCtrl', function($scope, backend) {
+  $scope.minAge = 18;
+  $scope.maxAge = 80;
+
   var perfectMatch = window.localStorage.getItem('perfectMatch');
   var preferredGender = window.localStorage.getItem('preferredGender');
   var distanceRadius = window.localStorage.getItem('distanceRadius');
+  var minAge = window.localStorage.getItem('minAge');
+  var maxAge = window.localStorage.getItem('maxAge');
 
   $scope.preference = {
     perfectMatch: (perfectMatch === 'true' ? true : false),
     preferredGender: preferredGender,
-    distanceRadius: (distanceRadius == null ? 50 : distanceRadius) //in km
+    distanceRadius: (distanceRadius == null ? 50 : distanceRadius), //in km
+    age: {
+      min: minAge === null ? $scope.minAge : minAge,
+      max: maxAge === null ? $scope.maxAge : maxAge,
+    }
   }
+
+  console.log($scope.preference);
 
   $scope.onChangePerfectMatch = function() {
     window.localStorage.setItem('perfectMatch', $scope.preference.perfectMatch);
