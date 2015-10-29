5ac4225850eb33783b67fd155d2020ef1cd460c1
Bili Xu <xubili93@gmail.com>
2015-10-30 00:36:48 +0800

    Add deal service

--- www/js/services/dealService.js
+++ www/js/services/dealService.js
@@ -1,4 +1,12 @@
 angular.module('letterbox.services')
 
 .service('DealService', function() {
+  var deal = this;
+
+  deal.currentDealCard = {};
+
+  deal.setCurrentDeal = function(deal) {
+    deal.currentDealCard = deal;
+  };
 });
+
