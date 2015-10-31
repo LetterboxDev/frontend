3440a23477a38503a79b1b33f87db55db2dbcf62
ianngiaw <ian@u.nus.edu>
2015-11-01 01:39:44 +0800

    Added method to show report popup

--- www/js/services/reportService.js
+++ www/js/services/reportService.js
@@ -1,11 +1,42 @@
 angular.module('letterbox.services')
 
-.service('ReportService', function($q, backend) {
-  return {
-    reportUser: function(userId, reason) {
-      var deferred = $q.defer();
-      backend.reportUser(userId, reason, deferred.resolve, deferred.reject);
-      return deferred.promise;
-    }
+.service('ReportService', function($q, $ionicPopup, backend) {
+  var ReportService = {};
+
+  ReportService.reportUser = function(userId, reason) {
+    var deferred = $q.defer();
+    backend.reportUser(userId, reason, deferred.resolve, deferred.reject);
+    return deferred.promise;
   };
+
+  ReportService.showReportPopup = function(userName, userId, $scope, callback) {
+    $scope.report = {
+      reason: ''
+    };
+    return $ionicPopup.show({
+      template: '<textarea ng-model="report.reason">',
+      title: 'Report ' + userName + '?',
+      subTitle: 'Please enter a reason behind this report',
+      scope: $scope,
+      buttons: [{
+        text: 'Cancel'
+      }, {
+        text: 'Report',
+        type: 'button-positive',
+        onTap: function(e) {
+          if (!$scope.report.reason) {
+            e.preventDefault();
+          } else {
+            ReportService.reportUser(userId, $scope.report.reason)
+            .then(function() {
+              $scope.report.reason = '';
+              callback();
+            });
+          }
+        }
+      }]
+    });
+  };
+
+  return ReportService;
 });
