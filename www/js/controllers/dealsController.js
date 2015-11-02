a26e5a94a9c86cde396fa7688184432d061d4a72
Bili Xu <xubili93@gmail.com>
2015-11-03 00:20:08 +0800

    Make deals page into a scrollable view

--- www/js/controllers/dealsController.js
+++ www/js/controllers/dealsController.js
@@ -2,10 +2,42 @@ angular.module('letterbox.controllers')
 
 .controller('DealsCtrl', function($scope,
                                   $state) {
-  $scope.deal = {
-    title: '1-for-1 at Carl\'s Jr',
-    image_url: 'http://lorempixel.com/250/250',
-    description: 'This is the best deal ever!'
-  };
+  $scope.deals = [
+    {
+      title: '1-for-1 at Carl\'s Jr',
+      image_url: 'http://lorempixel.com/250/250',
+      description: 'This is the best deal ever!'
+    },
+    {
+      title: '1-for-1 at Carl\'s Jr',
+      image_url: 'http://lorempixel.com/250/250',
+      description: 'This is the best deal ever!'
+    },
+    {
+      title: '1-for-1 at Carl\'s Jr',
+      image_url: 'http://lorempixel.com/250/250',
+      description: 'This is the best deal ever!'
+    },
+    {
+      title: '1-for-1 at Carl\'s Jr',
+      image_url: 'http://lorempixel.com/250/250',
+      description: 'This is the best deal ever!'
+    },
+    {
+      title: '1-for-1 at Carl\'s Jr',
+      image_url: 'http://lorempixel.com/250/250',
+      description: 'This is the best deal ever!'
+    },
+    {
+      title: '1-for-1 at Carl\'s Jr',
+      image_url: 'http://lorempixel.com/250/250',
+      description: 'This is the best deal ever!'
+    },
+    {
+      title: '1-for-1 at Carl\'s Jr',
+      image_url: 'http://lorempixel.com/250/250',
+      description: 'This is the best deal ever!'
+    }
+  ];
 });
 
