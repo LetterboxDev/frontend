2ec60490f4d74c6ddb669acfe4753577a3ba1ad1
Lei Mingyu <uygnim.iel@gmail.com>
2015-11-03 21:51:12 +0800

    add favorite deals controller

---
+++ www/js/controllers/favoriteDealsController.js
@@ -0,0 +1,45 @@
+angular.module('letterbox.controllers')
+
+.controller('FavoriteDealsCtrl', function($scope,
+                                          $rootScope,
+                                          $state,
+                                          DealCategoryService) {
+  $scope.favoriteDeals = [
+    {
+      title: '1-for-1 at Carl\'s Jr',
+      image_url: 'http://lorempixel.com/250/250',
+      description: 'This is the best deal ever!'
+    },
+    {
+      title: '1-for-1 at Carl\'s Jr',
+      image_url: 'http://lorempixel.com/250/250',
+      description: 'This is the best deal ever!'
+    },
+    {
+      title: '1-for-1 at Carl\'s Jr',
+      image_url: 'http://lorempixel.com/250/250',
+      description: 'This is the best deal ever!'
+    },
+    {
+      title: '1-for-1 at Carl\'s Jr',
+      image_url: 'http://lorempixel.com/250/250',
+      description: 'This is the best deal ever!'
+    },
+    {
+      title: '1-for-1 at Carl\'s Jr',
+      image_url: 'http://lorempixel.com/250/250',
+      description: 'This is the best deal ever!'
+    },
+    {
+      title: '1-for-1 at Carl\'s Jr',
+      image_url: 'http://lorempixel.com/250/250',
+      description: 'This is the best deal ever!'
+    },
+    {
+      title: '1-for-1 at Carl\'s Jr',
+      image_url: 'http://lorempixel.com/250/250',
+      description: 'This is the best deal ever!'
+    }
+  ];
+});
+
