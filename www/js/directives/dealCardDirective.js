4709ef49faeb1ac0a7a462990cfb5a9634ce2285
Bili Xu <xubili93@gmail.com>
2015-11-03 09:40:41 +0800

    Add deals view cards styling

--- www/js/directives/dealCardDirective.js
+++ www/js/directives/dealCardDirective.js
@@ -8,11 +8,6 @@ angular.module('letterbox.directives')
     restrict: 'EA',
     replace: true,
     controller: function($scope) {
-      $scope.nextCard = function() {
-        // TODO: hookup with backend
-        console.log('Next card.');
-      };
-
       $scope.favoriteCard = function() {
         // TODO: hookup with backend
         console.log('Favorite card.');
