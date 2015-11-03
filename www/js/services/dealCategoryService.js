0bf67cc4aaab67b62134639e5c9cf89a655071a0
ianngiaw <ian@u.nus.edu>
2015-11-03 22:22:41 +0800

    Added method to get all categories

--- www/js/services/dealCategoryService.js
+++ www/js/services/dealCategoryService.js
@@ -1,12 +1,18 @@
 angular.module('letterbox.services')
 
-.service('DealCategoryService', function() {
+.service('DealCategoryService', function($q, backend) {
   var dealCategory = this;
 
   dealCategory.currentCategory = '';
 
   dealCategory.setCurrentCategory = function(category) {
     dealCategory.currentCategory = category;
+  };
+
+  dealCategory.getCategories = function() {
+    var deferred = $q.defer();
+    backend.getDealCategories().then(deferred.resolve, deferred.reject);
+    return deferred.promise;
   };
 });
 
