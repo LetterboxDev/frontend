1b54c09a3f3f6f4e700f157ef48b9e36a59f8a03
Sebastian Quek <sebq13@gmail.com>
2015-11-06 21:23:06 +0800

    Updated controller to get deals from backend

--- www/js/controllers/dealsController.js
+++ www/js/controllers/dealsController.js
@@ -5,59 +5,12 @@ angular.module('letterbox.controllers')
                                   $state,
                                   DealCategoryService,
                                   DealService) {
-  $scope.deals = [
-    {
-      id: '1234',
-      title: '1-for-1 at Carl\'s Jr',
-      image_url: 'http://lorempixel.com/250/250',
-      description: 'This is the best deal ever!'
-    },
-    {
-      id: '1234',
-      title: '1-for-1 at Carl\'s Jr',
-      image_url: 'http://lorempixel.com/250/250',
-      description: 'This is the best deal ever!'
-    },
-    {
-      id: '1234',
-      title: '1-for-1 at Carl\'s Jr',
-      image_url: 'http://lorempixel.com/250/250',
-      description: 'This is the best deal ever!'
-    },
-    {
-      id: '1234',
-      title: '1-for-1 at Carl\'s Jr',
-      image_url: 'http://lorempixel.com/250/250',
-      description: 'This is the best deal ever!'
-    },
-    {
-      id: '1234',
-      title: '1-for-1 at Carl\'s Jr',
-      image_url: 'http://lorempixel.com/250/250',
-      description: 'This is the best deal ever!'
-    },
-    {
-      id: '1234',
-      title: '1-for-1 at Carl\'s Jr',
-      image_url: 'http://lorempixel.com/250/250',
-      description: 'This is the best deal ever!'
-    },
-    {
-      id: '1234',
-      title: '1-for-1 at Carl\'s Jr',
-      image_url: 'http://lorempixel.com/250/250',
-      description: 'This is the best deal ever!'
-    }
-  ];
 
   $scope.currentCategory = DealCategoryService.currentCategory;
 
-  // $scope.$on("$ionicView.enter", function(scopes, states) {
-  //   $scope.deals = [];
-  //   DealService.getDeals($scope.currentCategory).then(function(deals) {
-  //     $scope.deals = deals;
-  //   });
-  // });
+  DealService.getDeals($scope.currentCategory).then(function(deals) {
+    $scope.deals = deals;
+  });
 
   $scope.viewLiked = function() {
     $state.go('app.liked-deals');
