9e44791d73ddc499f1b137b81b2fe857a2fd819c
Bili Xu <xubili93@gmail.com>
2015-11-06 23:25:44 +0800

    Remove on view load since cache is already false

--- www/js/controllers/dealController.js
+++ www/js/controllers/dealController.js
@@ -7,11 +7,9 @@ angular.module('letterbox.controllers')
                                   DealService) {
   $scope.dealId = DealService.currentDealId;
 
-  $scope.$on("$ionicView.enter", function(scopes, states) {
-    DealService.getDeal($scope.dealId).then(function(deal) {
-      $scope.deal = deal;
-      $scope.deal.description = $sce.trustAsHtml($scope.deal.description);
-    });
+  DealService.getDeal($scope.dealId).then(function(deal) {
+    $scope.deal = deal;
+    $scope.deal.description = $sce.trustAsHtml($scope.deal.description);
   });
 });
 
