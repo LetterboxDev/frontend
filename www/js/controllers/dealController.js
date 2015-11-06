be6c1079e27f912d0337b020ea3b0e3037e7d287
Sebastian Quek <sebq13@gmail.com>
2015-11-06 22:34:09 +0800

    Deal is now retrieved from backend

--- www/js/controllers/dealController.js
+++ www/js/controllers/dealController.js
@@ -5,13 +5,11 @@ angular.module('letterbox.controllers')
                                   $state,
                                   DealService) {
   $scope.dealId = DealService.currentDealId;
-  $scope.deal = {
-    title: 'Carl\'s Jr 1-for-1',
-    image: 'http://lorempixel.com/300/300',
-    description: 'Best deal ever!',
-    location: 'ION Orchard',
-    expiry: '2015/05/21',
-    provider: 'NUStyles'
-  };
+
+  $scope.$on("$ionicView.enter", function(scopes, states) {
+    DealService.getDeal($scope.dealId).then(function(deal) {
+      $scope.deal = deal;
+    });
+  });
 });
 
