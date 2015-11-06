fd30148b6cf4a55eb2241cc0bd403a287241ba1b
Sebastian Quek <sebq13@gmail.com>
2015-11-06 22:36:08 +0800

    Updated template, deal description can contain html

--- www/js/controllers/dealController.js
+++ www/js/controllers/dealController.js
@@ -3,12 +3,14 @@ angular.module('letterbox.controllers')
 .controller('DealCtrl', function($scope,
                                   $rootScope,
                                   $state,
+                                  $sce,
                                   DealService) {
   $scope.dealId = DealService.currentDealId;
 
   $scope.$on("$ionicView.enter", function(scopes, states) {
     DealService.getDeal($scope.dealId).then(function(deal) {
       $scope.deal = deal;
+      $scope.deal.description = $sce.trustAsHtml($scope.deal.description);
     });
   });
 });
