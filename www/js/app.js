1dd2e21c451aa8121363926f8080141138c90225
ianngiaw <ian@u.nus.edu>
2015-11-07 16:25:31 +0800

    Added version number constant

--- www/js/app.js
+++ www/js/app.js
@@ -18,6 +18,12 @@ angular.module('letterbox', ['ionic',
                              'ngIOS9UIWebViewPatch'
                             ])
 
+.constant('VERSION', {
+  major: 0,
+  minor: 1,
+  revision: 0  
+})
+
 .run(function($ionicPlatform, $rootScope, eventbus) {
   $rootScope.$on("$stateChangeError", console.log.bind(console));
 
@@ -57,4 +63,4 @@ angular.module('letterbox', ['ionic',
 
 .config(function(FacebookProvider) {
   FacebookProvider.init('1674828996062928');
-})
+});
