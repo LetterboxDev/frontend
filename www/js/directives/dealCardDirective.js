936b3a30e630a191591bc940f6540258dffdc170
ianngiaw <ian@u.nus.edu>
2015-11-08 18:53:07 +0800

    Add deal thumbnail image in deal service

--- www/js/directives/dealCardDirective.js
+++ www/js/directives/dealCardDirective.js
@@ -8,13 +8,6 @@ angular.module('letterbox.directives')
     restrict: 'EA',
     replace: true,
     controller: function($scope, $state, DealService) {
-      var imgurImageSize = "b";
-      var imageUrl = $scope.ctrl.data.images[0];
-      var dotIndex = imageUrl.lastIndexOf('.');
-      $scope.smallImage = imageUrl.substr(0, dotIndex) + 
-                          imgurImageSize + 
-                          imageUrl.substr(dotIndex);
-
       $scope.learnMore = function(deal) {
         DealService.setCurrentDeal(deal);
         $state.go('app.deal');
