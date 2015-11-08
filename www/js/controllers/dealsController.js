51edfcf1fc77ac77432437bf48bd1f0e88517ace
Sebastian Quek <sebq13@gmail.com>
2015-11-08 19:11:37 +0800

    Removed on ionicView.enter so that going from deal to deals doesnt refresh

--- www/js/controllers/dealsController.js
+++ www/js/controllers/dealsController.js
@@ -8,21 +8,19 @@ angular.module('letterbox.controllers')
 
   $scope.currentCategory = DealCategoryService.currentCategory;
 
-  $scope.$on('$ionicView.enter', function() {
-    $scope.isLoading = true;
-    if ($scope.currentCategory === "Featured") {
-      DealService.getFeaturedDeals().then(function(deals) {
-        $scope.deals = deals;
-        $scope.isLoading = false;
-      });
-    } else {
-      DealService.getDeals($scope.currentCategory).then(function(deals) {
-        $scope.deals = deals;
-        $scope.isLoading = false;
-      });
-    }
-  });
-
+  $scope.isLoading = true;
+  if ($scope.currentCategory === "Featured") {
+    DealService.getFeaturedDeals().then(function(deals) {
+      $scope.deals = deals;
+      $scope.isLoading = false;
+    });
+  } else {
+    DealService.getDeals($scope.currentCategory).then(function(deals) {
+      $scope.deals = deals;
+      $scope.isLoading = false;
+    });
+  }
+  
   $scope.viewLiked = function() {
     $state.go('app.liked-deals');
   }
