76ec61a399fb3213edc0ea1402f29dd0093a1055
Lei Mingyu <uygnim.iel@gmail.com>
2015-11-08 23:24:15 +0800

    add link to deal view

--- www/js/directives/profileCardDirective.js
+++ www/js/directives/profileCardDirective.js
@@ -3,9 +3,15 @@ angular.module('letterbox.directives')
 .directive('profileCard', function() {
   return {
     scope: {},
-    controller: ['$scope', 'ReportService', function($scope, ReportService) {
+    controller: ['$scope', 'ReportService', '$state', 'DealService', function($scope, ReportService, $state, DealService) {
       $scope.reportUser = function(userName, userId, callback) {
         ReportService.showReportPopup(userName, userId, $scope, callback);
+      }.bind(this);
+
+      $scope.viewDeal = function(deal) {
+        DealService.setCurrentDeal(deal);
+        DealService.showShare = true;
+        $state.go('app.deal');
       }.bind(this);
 
       $scope.likedDeals = [
