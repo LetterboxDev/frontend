0f6182bbec5e3b98427b78ff6615b1dc643b1d8a
Sebastian Quek <sebq13@gmail.com>
2015-11-08 15:16:51 +0800

    Added ability to zoom into pictures in deal view

--- www/js/controllers/dealController.js
+++ www/js/controllers/dealController.js
@@ -5,6 +5,9 @@ angular.module('letterbox.controllers')
                                   $state,
                                   $sce,
                                   $ionicHistory,
+                                  $ionicModal,
+                                  $ionicScrollDelegate,
+                                  $ionicSlideBoxDelegate,
                                   DealService) {
 
   $scope.dealShareButton = false;
@@ -17,5 +20,32 @@ angular.module('letterbox.controllers')
   };
 
   $scope.deal = DealService.currentDeal;
+  $scope.zoomMin = 1;
+
+  $scope.showImages = function(index) {
+    $scope.activeSlide = index;
+    $ionicModal.fromTemplateUrl('templates/deal-images-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+      $scope.modal.show();
+    });
+  };
+
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+    $scope.modal.remove();
+  };
+
+  $scope.updateSlideStatus = function(slide) { // ensures you cannot switch pictures when zoomed in
+    var zoomFactor = $ionicScrollDelegate.$getByHandle('scrollHandle' + slide).getScrollPosition().zoom;
+    if (zoomFactor == $scope.zoomMin) {
+      $ionicSlideBoxDelegate.enableSlide(true);
+    } else {
+      $ionicSlideBoxDelegate.enableSlide(false);
+    }
+  };
+
 });
 
