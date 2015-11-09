eb714e5f5a0a8ae09c5a98dc4be9b6bf4875d3e2
Bili Xu <xubili93@gmail.com>
2015-11-09 23:08:17 +0800

    Add profile picture modal to all small profile pictures

--- www/js/directives/profilePicSmallDirective.js
+++ www/js/directives/profilePicSmallDirective.js
@@ -7,7 +7,21 @@ angular.module('letterbox.directives')
     },
     restrict: 'EA',
     replace: true,
-    controller: function($scope) {
+    controller: function($scope, $ionicModal) {
+      $scope.showProfilePic = function() {
+        $ionicModal.fromTemplateUrl('templates/profile-pic-modal.html', {
+          scope: $scope,
+          animation: 'slide-in-up'
+        }).then(function(modal) {
+          $scope.modal = modal;
+          $scope.modal.show();
+        });
+      };
+
+      $scope.closeModal = function() {
+        $scope.modal.hide();
+        $scope.modal.remove();
+      };
     },
     controllerAs: 'ctrl',
     bindToController: true,
