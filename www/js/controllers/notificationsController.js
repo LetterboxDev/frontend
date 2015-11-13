f2d5d256ba7b4b5aeadca09cb8482a984ffb46c6
Lei Mingyu <uygnim.iel@gmail.com>
2015-11-14 01:32:16 +0800

    add the whole chats view in notifications

--- www/js/controllers/notificationsController.js
+++ www/js/controllers/notificationsController.js
@@ -4,11 +4,14 @@ angular.module('letterbox.controllers')
                                           $state,
                                           $ionicModal,
                                           NotificationsService,
+                                          RoomsService,
                                           eventbus,
                                           backend) {
 
   $scope.notifications = [];
+  $scope.chats = [];
   $scope.isLoading = false;
+  $scope.viewingNotifications = 'letters';
 
   function refreshNotifications() {
     $scope.isLoading = true;
@@ -18,9 +21,69 @@ angular.module('letterbox.controllers')
     });
   }
 
+  function formatChat(room) {
+    var latestMessage = room.latestMessage;
+    var last_message = latestMessage.content ? (latestMessage.sender === window.localStorage.getItem('hashedId') ? 'You: ' : room.userName + ': ') + latestMessage.content : 'No messages yet';
+    var last_activity = latestMessage.timeSent ? new Date(latestMessage.timeSent) : new Date(room.createdAt);
+    return {
+      id: room.hash,
+      from: room.userName,
+      profile_pic: room.thumbnail,
+      last_message: last_message,
+      last_activity: last_activity
+    };
+  }
+
+  function updateRooms(rooms) {
+    for (var i = 0; i < rooms.length; i++) {
+      var room = rooms[i];
+      RoomsService.getLatestRoomInfo(room.hash).then(function(room) {
+        var chat = formatChat(room);
+        var chatModified = false;
+        for (var j = 0; j < $scope.chats.length; j++) {
+          if ($scope.chats[j].id === chat.id) {
+            $scope.chats[j] = chat;
+            chatModified = true;
+            break;
+          }
+        }
+        if (!chatModified) {
+          $scope.chats.push(chat);
+        }
+        $scope.chats.sort(function(a, b) {
+          return b.last_activity.getTime() - a.last_activity.getTime();
+        });
+      });
+    }
+    $scope.$broadcast('scroll.refreshComplete');
+  }
+
+  $scope.refreshRooms = function() {
+    RoomsService.updateRooms();
+  };
+
+  eventbus.registerListener('roomsUpdated', updateRooms);
+
+  eventbus.registerListener('roomMessage', function(roomMessage) {
+    var message = roomMessage.message;
+    for (var i = 0; i < $scope.chats.length; i++) {
+      if ($scope.chats[i].id === message.RoomHash) {
+        $scope.chats[i].last_message = (message.sender === window.localStorage.getItem('hashedId') ? 'You: ' : $scope.chats[i].from + ': ') + message.content;
+        $scope.chats[i].last_activity = new Date(message.timeSent);
+        break;
+      }
+    }
+    $scope.chats.sort(function(a, b) {
+      return b.last_activity.getTime() - a.last_activity.getTime();
+    });
+    $scope.$apply();
+  });
+
   eventbus.registerListener('letterReceived', refreshNotifications);
+
   $scope.$on("$ionicView.enter", function(scopes, states) {
     refreshNotifications();
+    RoomsService.updateRooms();
   });
 
   $scope.selectNotification = function(notification) {
