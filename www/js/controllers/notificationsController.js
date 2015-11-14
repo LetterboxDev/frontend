80244982baca8593f453fcce1cdadf85a56167e1
ianngiaw <ian@u.nus.edu>
2015-11-14 23:14:08 +0800

    Update chats data individually in notifications page

--- www/js/controllers/notificationsController.js
+++ www/js/controllers/notificationsController.js
@@ -47,7 +47,9 @@ angular.module('letterbox.controllers')
         var chatModified = false;
         for (var j = 0; j < $scope.chats.length; j++) {
           if ($scope.chats[j].id === chat.id) {
-            $scope.chats[j] = chat;
+            $scope.chats[j].last_message = chat.last_message;
+            $scope.chats[j].last_activity = chat.last_activity;
+            $scope.chats[j].unread_count = chat.unread_count;
             chatModified = true;
             break;
           }
