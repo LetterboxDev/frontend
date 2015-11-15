a070fb5b9383a20eda22e42fe4110005b3c35677
ianngiaw <ian@u.nus.edu>
2015-11-15 17:26:06 +0800

    Moved all code related to getting matches into MatchService, allows for gender preference switching

--- www/js/controllers/cardsController.js
+++ www/js/controllers/cardsController.js
@@ -20,36 +20,37 @@ angular.module('letterbox.controllers')
   $scope.requestingNumber = 0;
 
   eventbus.registerListener('enterHome', checkAndGetCard);
-  eventbus.registerListener('changeGender', checkAndGetCard);
-  // eventbus.registerListener('closeLetter', removeTopCard);
+  eventbus.registerListener('changeGender', changeGender);
+  eventbus.registerListener('closeLetter', $scope.changeCard);
 
-  getInitialCards();
+  getInitialCard();
 
   $scope.changeCard = function() {
     if ($scope.isCardAnimating) {
       return;
     }
-
-    if ($scope.requestingNumber === 0) {
-      $scope.addCard();
-    }
-
     $scope.isCardAnimating = true;
     selectFirst('.profile-card').addClass('moving-out');
 
     $timeout(function() {
       // timeout for moving out animation
       selectFirst('.profile-card').removeClass('moving-out');
-      selectFirst('.profile-card').addClass('moving-in');
-      $scope.cards.splice(0, 1);
-      $scope.isLoading = false;
-      $scope.isCardAnimating = false;
-
-      // timeout for remove moving in animation
-      $timeout(function() {
-        selectFirst('.profile-card').removeClass('moving-in');
-      }, 500);
-
+      $scope.isLoading = true;
+      MatchService.getCard().then(function(card) {
+        $scope.cards.splice(0, 1);
+        $scope.cards.push(card);
+        selectFirst('.profile-card').addClass('moving-in');
+        $scope.isLoading = false;
+        $scope.isCardAnimating = false;
+        // timeout for remove moving in animation
+        $timeout(function() {
+          selectFirst('.profile-card').removeClass('moving-in');
+        }, 500);
+      }, function() {
+        $scope.cards.splice(0, 1);
+        $scope.isLoading = false;
+        $scope.isCardAnimating = false;
+      });
     }, 500);
   };
 
@@ -62,15 +63,6 @@ angular.module('letterbox.controllers')
     $state.go('app.letter');
   };
 
-  $scope.addCard = function() {
-    if (!$scope.isLoading) {
-      MatchService.getMatch()
-      .then(function(match) {
-        $scope.cards.push(createNewCard(match));
-      });
-    }
-  };
-
   /**
    * Helper functions
    */
@@ -78,70 +70,33 @@ angular.module('letterbox.controllers')
     // if there is card currently loaded, change it
     // otherwise, load new card
     if ($scope.cards.length === 0) {
-      getInitialCards();
+      getInitialCard();
+    }
+  }
+
+  function changeGender() {
+    if ($scope.cards.length === 0) {
+      getInitialCard();
     } else {
       $scope.changeCard();
     }
   }
 
-  function getInitialCards() {
+  function getInitialCard() {
     if (window.localStorage.getItem('token') && !$scope.isLoading) {
       $scope.isLoading = true;
-
-      for (i = 0; i < bufferSize; i ++) {
-        $scope.requestingNumber ++;
-        // avoid concurrency issue
+      MatchService.getCard().then(function(card) {
+        $scope.isLoading = false;
+        $scope.cards.push(card);
+        $scope.isCardAnimating = true;
         $timeout(function() {
-          MatchService.getMatch()
-          .then(function(match) {
-            $scope.requestingNumber --;
-
-            if ($scope.cards.length === 2) {
-              // temp hack
-              $scope.isLoading = false;
-              $timeout(function() {
-                selectFirst('.profile-card').removeClass('moving-in');
-              }, 500);
-            }
-
-            if (match) {
-              var duplicatedMatch = false;
-
-              $scope.cards.forEach(function(matched) {
-                if (matched.hashedId === match.hashedId) {
-                  duplicatedMatch = true;
-                }
-              });
-
-              if (!duplicatedMatch) {
-                $scope.cards.push(createNewCard(match));
-              }
-            }
-
-          }, function() {
-            $scope.isLoading = false;
-          });
-        }, i * 800);
-      }
+          selectFirst('.profile-card').removeClass('moving-in');
+          $scope.isCardAnimating = false;
+        }, 500);
+      }, function() {
+        $scope.isLoading = false;
+      });
     }
-  }
-
-  function removeTopCard() {
-    $scope.cards.shift();
-  }
-
-  function createNewCard(match) {
-    return {
-      hashedId: match.hashedId,
-      name: match.firstName,
-      age: match.age,
-      location: Math.floor(match.distance) + 'km',
-      bio: match.bio,
-      profile_pic: match.pictureMed,
-      questions: match.questions,
-      mutual_friends_count: (typeof match.mutualFriends === 'undefined') ? 'unknown' : match.mutualFriends.summary.total_count,
-      likedDeals: match.likedDeals
-    };
   }
 
   function selectFirst(selector) {
--- www/js/services/matchService.js
+++ www/js/services/matchService.js
@@ -2,10 +2,21 @@ angular.module('letterbox.services')
 
 .service('MatchService', function($q,
                                   $ImageCacheFactory,
+                                  $timeout,
+                                  $rootScope,
                                   backend,
                                   AuthService) {
 
   var MatchService = this;
+  var maleMatches = [];
+  var femaleMatches = [];
+  var outstandingRequests = 0;
+  var isMaleInit = false;
+  var isFemaleInit = false;
+
+  var MAX_BUFFER = 20;
+  var REQUEST_INTERVAL = 800;
+  var INITIAL_REQUESTS = 4;
 
   MatchService.previousId = '';
 
@@ -37,4 +48,137 @@ angular.module('letterbox.services')
     }
     return deferred.promise;
   };
+
+  MatchService.getInitialMatch = function() {
+    var deferred = $q.defer();
+    var totalCompleted = 0;
+    var resolved = false;
+    var seen = [];
+    var matches = window.localStorage.getItem('genderPreference') === 'male' ? maleMatches : femaleMatches;
+    if (window.localStorage.getItem('genderPreference') === 'male') {
+      isMaleInit = true;
+    } else {
+      isFemaleInit = true;
+    }
+    if (outstandingRequests === 0 && matches.length && AuthService.isRegistered()) {
+      var match = matches.shift();
+      deferred.resolve(match);
+    } else if (AuthService.isRegistered()) {
+      for (var i = 0; i < MAX_BUFFER; i++) {
+        $timeout(function() {
+          outstandingRequests++;
+          MatchService.getMatch().then(function(match) {
+            outstandingRequests--;
+            totalCompleted++;
+            if (seen.indexOf(match.hashedId) === -1) {
+              seen.push(match.hashedId);
+              matches.push(match);
+            }
+            if (totalCompleted >= INITIAL_REQUESTS && matches.length && !resolved) {
+              resolved = true;
+              deferred.resolve(matches.shift());
+            } else if (totalCompleted >= INITIAL_REQUESTS && !matches.length && !resolved) {
+              resolved = true;
+              deferred.reject();
+            }
+          }, function() {
+            outstandingRequests--;
+            totalCompleted++;
+            if (totalCompleted >= INITIAL_REQUESTS && matches.length && !resolved) {
+              resolved = true;
+              deferred.resolve(matches.shift());
+            } else {
+              resolved = true;
+              deferred.reject();
+            }
+          });
+        }, i*REQUEST_INTERVAL);
+      }
+    } else {
+      deferred.reject();
+    }
+    return deferred.promise;
+  };
+
+  MatchService.getNextMatch = function() {
+    var deferred = $q.defer();
+    var matches = window.localStorage.getItem('genderPreference') === 'male' ? maleMatches : femaleMatches;
+    if (matches.length) {
+      var match = matches.shift();
+      outstandingRequests++;
+      MatchService.getMatch().then(function(matchedUser) {
+        outstandingRequests--;
+        matches.push(matchedUser);
+      }, function() {
+        outstandingRequests--;
+      });
+      deferred.resolve(match);
+    } else if (outstandingRequests) {
+      $rootScope.$watch(function() {
+        return outstandingRequests;
+      }, function(newValue, oldValue) {
+        if (newValue === 0 && oldValue > 0) {
+          if (matches.length) {
+            outstandingRequests++;
+            MatchService.getMatch().then(function(matchedUser) {
+              outstandingRequests--;
+              matches.push(matchedUser);
+            }, function() {
+              outstandingRequests--;
+            });
+            deferred.resolve(match);
+          } else {
+            deferred.reject();
+          }
+        }
+      });
+    } else {
+      MatchService.getMatch().then(function(matchedUser) {
+        deferred.resolve(match);
+      }, function() {
+        deferred.reject();        
+      })
+    }
+    return deferred.promise;
+  }
+
+  MatchService.getCard = function() {
+    var deferred = $q.defer();
+    if (window.localStorage.getItem('genderPreference') === 'male') {
+      if (!isMaleInit) {
+        MatchService.getInitialMatch().then(function(match) {
+          deferred.resolve(createNewCard(match));
+        }, deferred.reject);
+      } else {
+        MatchService.getNextMatch().then(function(match) {
+          deferred.resolve(createNewCard(match));
+        }, deferred.reject);
+      }
+    } else {
+      if (!isFemaleInit) {
+        MatchService.getInitialMatch().then(function(match) {
+          deferred.resolve(createNewCard(match));
+        }, deferred.reject);
+      } else {
+        MatchService.getNextMatch().then(function(match) {
+          deferred.resolve(createNewCard(match));
+        }, deferred.reject);
+      }
+    }
+    return deferred.promise;
+  };
+
+  function createNewCard(match) {
+    return {
+      hashedId: match.hashedId,
+      name: match.firstName,
+      age: match.age,
+      location: Math.floor(match.distance) + 'km',
+      bio: match.bio,
+      profile_pic: match.pictureMed,
+      questions: match.questions,
+      mutual_friends_count: (typeof match.mutualFriends === 'undefined') ? 'unknown' : match.mutualFriends.summary.total_count,
+      likedDeals: match.likedDeals
+    };
+  }
 });
